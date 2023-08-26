23f10d85f389f01a268a7181fe8a0aba272ef189
Natthapol-PEET <natthapol.n@ku.th>
2023-08-27 00:39:17 +0700

    fix-test-camera-r2

--- tests/camera.py
+++ tests/camera.py
@@ -1,9 +1,12 @@
-import cv2 as cv
+import cv2
+import time
 import sys
+
+count = 0
 
 index = sys.argv[1]
 print(f'camera index {index}')
-cap = cv.VideoCapture(int(index))
+cap = cv2.VideoCapture(int(index))
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -13,14 +16,15 @@ while True:
     ret, frame = cap.read()
 
     if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+        count += 1
+        print(f'Can\'t receive frame (stream end?). Exiting ... {count}')
+        time.sleep(3)
         continue
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
+    cv2.imshow('frame', frame)
 
-    if cv.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
 
     cap.release()
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
